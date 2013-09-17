5054ee30daa8adb1c3d9fcf6c242eb04f73f72ec
Rahul Naik <rahul.naik.prdxn@gmail.com>
2013-09-17 15:35:15 +0530

    push file changed

--- push.php
+++ push.php
@@ -1,4 +1,17 @@
-<?php
-$output = shell_exec('git pull origin master');
-echo "<pre>$output</pre>";
+<?php 
+/**
+* Git Pull
+*
+* @author Adam Patterson
+* http://www.adampatterson.ca/blog/2011/10/diy-simple-staging-server/
+*
+* Use: echo pull();
+*/
+ 
+function pull ( )
+{
+return shell_exec('git pull');
+}
+
+echo pull();
 ?>
