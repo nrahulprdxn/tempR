b17bfe2bac56dbc02082eee4633e64350f091965
Rahul Naik <rahul.naik.prdxn@gmail.com>
2013-09-18 17:45:31 +0530

    new code added

--- push.php
+++ push.php
@@ -1,10 +1,9 @@
-#!php -q
+
 <?php 
 /**
 * Git Pull
 */
  
-   echo shell_exec('git pull origin master') . "<br /><br />"; 
+   echo shell_exec('git pull origin master'); 
 
-   echo shell_exec('git status');
 ?>
