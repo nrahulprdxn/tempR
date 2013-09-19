b3ca79566cd11ef6b94a8bdd413653071168c948
Rahul Naik <rahul.naik.prdxn@gmail.com>
2013-09-19 17:11:45 +0530

    Made configuration

--- config.php
+++ config.php
@@ -4,11 +4,11 @@
 $baseurl = 'http://localhost/webvantage/';
 
 /* Set database variables here */
-$host = 'localhost';     // Set host name of database
-$username = 'root';      // Set database username
-$password = '';          // Set database password
-$dbname = 'webvantage';  // Set database name
-$dbpre = 'in';           // Set table prefix
+$host = getenv("IP");            // Set host name of database
+$username = 'nrahulprdxn';      // Set database username
+$password = '';                // Set database password
+$dbname = 'c9';               // Set database name
+$dbpre = 'in';               // Set table prefix
 
 /* Connection to database */
 $mysqli = new mysqli($host, $username, $password, $dbname);
