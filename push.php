c5c4d850fff49e6dc9707b9a5cd8018538239ac2
Rahul Naik <rahul.naik.prdxn@gmail.com>
2013-09-19 17:19:46 +0530

    Read me edits

--- push.php
+++ push.php
@@ -1,9 +1,18 @@
+<html>
+  <head>
+    
+  </head>
+</html>
+
+
+
+
 
 <?php 
 /**
 * Git Pull
 */
- 
+
 exec('git pull origin master', $shell_output, $output);
 print_r($shell_output);
 print_r($output);
