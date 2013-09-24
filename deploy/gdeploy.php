aff3b8381af6981132555f318b48885c8da96cd1
Rahul Naik <rahul.naik.prdxn@gmail.com>
2013-09-24 15:24:18 +0530

    added password

--- deploy/gdeploy.php
+++ deploy/gdeploy.php
@@ -1,7 +1,7 @@
 <?php
   
 exec('cd var/www/test/tempR');  
-exec('git pull origin master 2>&1', $retval, $output);
+exec('sudo git pull origin master 2>&1; prdxn2012', $retval, $output);
 echo trim(shell_exec('whoami'));
 //system('echo "prdxn2012" | sudo -u user -S git pull origin master 2>&1', $retval);
 
