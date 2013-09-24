9616a2f21a3944ef878afeb19516a0289e955580
Rahul Naik <rahul.naik.prdxn@gmail.com>
2013-09-24 18:31:13 +0530

    home file updated

--- home.php
+++ home.php
@@ -8,3 +8,56 @@
 <div>third div structure!</div>
 </body>
 </html>
+
+<?php
+
+$host = 'localhost';            // Set host name of database
+$username = 'root';      // Set database username
+$password = 'prdxn2012';                // Set database password
+$dbname = 'test';               // Set database name
+$dbpre = 'in';               // Set table prefix
+
+$q = "SHOW TABLES";
+$op = $mysqli->query($q);
+echo "<div class='table-list cf'>
+  <p style='color: #fff'>List of Tables</p>
+      <div>";
+while ($row = mysqli_fetch_array($op)) {
+  
+   echo $row['Tables_in_c9'];
+   
+
+  echo '<div class="view-table">';
+
+  echo $qu = "SELECT * FROM " . $row['Tables_in_c9'];
+  $results = $mysqli->query($qu);
+  echo '<table>';
+  if(count($results) > 0){
+  	foreach ( mysqli_fetch_array($results) as $index => $result) {
+      if($index == 0){
+     echo '<tr class="dbtable_wrapper">';
+    
+      foreach ($result as $key => $value) {
+        
+        echo '<td class="user_id">'. $key .'</td>';
+        
+      }
+      echo '</tr>';
+    }
+      echo '<tr class="dbtable_wrapper">';
+    
+      foreach ($result as $key => $value) {
+        
+        echo '<td class="user_id">' . $value . '</td>';
+        
+      }
+      echo '</tr>';
+    }
+  }
+  else{
+    echo '<tr class="dbtable_wrapper"><td class="user_id">No data</td></tr>';
+  }
+   echo '</table>';
+}
+
+?>
