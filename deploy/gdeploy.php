398d1527f59f6e1423a191884e0526a81ae08398
Rahul Naik <rahul.naik.prdxn@gmail.com>
2013-09-24 13:40:32 +0530

    added error return value

--- deploy/gdeploy.php
+++ deploy/gdeploy.php
@@ -2,7 +2,9 @@
   
 exec('cd var/www/test/tempR');  
 //exec('git pull origin master', $shell_output, $output);
-system('echo "prdxn2012" | sudo -u user -S git pull origin master');
+system('echo "prdxn2012" | sudo -u user -S git pull origin master', $retval);
+
+echo $retval;
 
 ?>
   
