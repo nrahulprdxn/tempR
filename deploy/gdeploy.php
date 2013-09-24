7bc419d0c990baa8531f1278673c917e612122fc
Rahul Naik <rahul.naik.prdxn@gmail.com>
2013-09-24 14:13:12 +0530

    changed cmd

--- deploy/gdeploy.php
+++ deploy/gdeploy.php
@@ -1,7 +1,7 @@
 <?php
   
 exec('cd var/www/test/tempR');  
-exec('git pull origin master', $retval, $output);
+exec('git pull origin master 2>&1', $retval, $output);
 //system('echo "prdxn2012" | sudo -u user -S git pull origin master 2>&1', $retval);
 
 echo $retval;
